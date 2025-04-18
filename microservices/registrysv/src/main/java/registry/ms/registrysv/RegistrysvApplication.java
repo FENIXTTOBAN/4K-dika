fcc1b280662d09415d9bbc1adfe6b501457715c0
FENIXTTOBAN <dani.saavedra1406@gmail.com>
2025-04-18 00:57:09 -0500

    Realizacion del RegistrySV check

--- microservices/registrysv/src/main/java/registry/ms/registrysv/RegistrysvApplication.java
+++ microservices/registrysv/src/main/java/registry/ms/registrysv/RegistrysvApplication.java
@@ -2,8 +2,10 @@ package registry.ms.registrysv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class RegistrysvApplication {
 
 	public static void main(String[] args) {
