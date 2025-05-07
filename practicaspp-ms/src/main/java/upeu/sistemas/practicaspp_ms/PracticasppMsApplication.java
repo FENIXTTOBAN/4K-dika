925194a6aef910f47acf8ddf8885cab061af019a
FENIXTTOBAN <dani.saavedra1406@gmail.com>
2025-05-07 18:19:31 -0500

    Agregando el microservicio

---
+++ practicaspp-ms/src/main/java/upeu/sistemas/practicaspp_ms/PracticasppMsApplication.java
@@ -0,0 +1,13 @@
+package upeu.sistemas.practicaspp_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticasppMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticasppMsApplication.class, args);
+	}
+
+}
---
+++ practicaspp-ms/src/test/java/upeu/sistemas/practicaspp_ms/PracticasppMsApplicationTests.java
@@ -0,0 +1,13 @@
+package upeu.sistemas.practicaspp_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticasppMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
