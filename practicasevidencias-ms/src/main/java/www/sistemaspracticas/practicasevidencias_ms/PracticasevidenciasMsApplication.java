07e673fadc520122a4b597afe0e70a3db97011c8
FENIXTTOBAN <dani.saavedra1406@gmail.com>
2025-06-02 01:57:12 -0500

    todo funcional con eureka y ports 0

--- practicasevidencias-ms/src/main/java/www/sistemaspracticas/practicasevidencias_ms/PracticasevidenciasMsApplication.java
+++ practicasevidencias-ms/src/main/java/www/sistemaspracticas/practicasevidencias_ms/PracticasevidenciasMsApplication.java
@@ -2,8 +2,10 @@ package www.sistemaspracticas.practicasevidencias_ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PracticasevidenciasMsApplication {
 
 	public static void main(String[] args) {
